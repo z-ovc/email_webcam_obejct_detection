defaff9a171813c0f9c5dc5e8218bf5eef62c334
Zahra Oveisi <z.oveisi@ebcom.ir>
2023-07-05 12:09:18 +0330

    get the frame to send email

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import time
+from emailing import send_email
+
 
 
 video = cv2.VideoCapture(0)
@@ -7,8 +9,9 @@ time.sleep(1)
 
 
 first_farme = None
-
+status_list = []
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame,(21,21),0)
@@ -26,7 +29,17 @@ while True:
         if cv2.contourArea(contour)<5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),3)
+        rectangle = cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),3)
+        if rectangle.any():
+            status = 1
+            
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0]==1 and status_list[1]==0:
+        send_email()
+
+
     cv2.imshow("video",frame)
     key = cv2.waitKey(1)
     if key == ord("q"):
